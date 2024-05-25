3f2e4ba1b86903642884b3e9144692ce474d4fa5
keerthivasan <keerthivasan280901@gmail.com>
2024-05-25 14:47:41 +0530

    Integration

---
+++ LXP.Data/IRepository/IEnrollmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using LXP.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LXP.Data.IRepository
+{
+    public interface IEnrollmentRepository
+    {
+        Task Addenroll(Enrollment enrollment);
+
+        bool AnyEnrollmentByLearnerAndCourse(Guid learnerId, Guid courseId);
+
+        object GetCourseandTopicsByLearnerId(Guid learnerId);
+    }
+}
